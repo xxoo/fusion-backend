8e794f8096e2923a7b9b3acb5954ae248927d0c0
Xiao Shen <dead.xx@gmail.com>
2018-01-23 18:29:51 +0800

    no message

--- captchaServer.js
+++ captchaServer.js
@@ -2,91 +2,87 @@
 const fs = require('fs'),
 	path = require('path'),
 	net = require('net'),
-	opentype = require('opentype.js'),
-	font = opentype.loadSync(path.join(__dirname, 'captcha.ttf'));
+	font = require('opentype.js').loadSync(path.join(__dirname, 'captcha.ttf')),
+	config = require('./config.js');
 require('./jsex.js');
-eval(fs.readFileSync(path.join(__dirname, 'config.js'), {
-	encoding: 'utf8'
-}))(function (config) {
-	let f = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-		props = ['x', 'y', 'x1', 'y1'],
-		stats = {
-			clients: 0,
-			requests: 0
-		},
-		tps = {
-			all: f.length - 1,
-			normal: 35,
-			number: 9
-		},
-		startServer = function (err) {
-			if (err) {
-				console.log('server is stoped by error, restarting...');
-			}
-			net.createConnection(config.server.captcha, function () {
-				this.end();
-				console.log('server is already running');
-			}).on('error', function () {
-				if (typeof config.server.captcha === 'string') {
-					fs.unlink(config.server.captcha, function () {
-						server.listen(config.server.captcha);
-					});
-				} else {
-					server.listen(config.server.captcha);
-				}
-			});
-		},
-		server = net.createServer(function (socket) {
-			let msg = '';
-			socket.on('data', function (data) {
-				data = data.split('\n');
-				for (let i = 0; i < data.length; i++) {
-					msg += data[i];
-					if (i < data.length - 1) {
-						msg = msg.parseJsex();
-						if (msg && dataType(msg.value) === 'object') {
-							let len = msg.value.len || 4,
-								t = tps[msg.value.type] || tps.normal,
-								s = '';
-							for (let i = 0; i < len; i++) {
-								s += f[Math.round(Math.random() * t)];
-							}
-							t = font.getPath(s);
-							t.commands.forEach(function (cmd) {
-								props.forEach(function (v) {
-									if (cmd.hasOwnProperty(v)) {
-										cmd[v] += Math.random() * 4 - 2;
-									}
-								});
-							});
-							msg = {
-								path: t.toPathData(),
-								text: s
-							};
-						} else {
-							msg = Error('bad request');
-						}
-						socket.write(toJsex(msg) + '\n');
-						stats.requests++;
-						msg = '';
-					}
-				}
-			}).on('close', function () {
-				stats.clients--;
-			}).setEncoding('utf8');
-			stats.clients++;
-		}).on('listening', function () {
-			console.log('server started.');
-		}).on('error', function (err) {
-			console.error(err.stack);
-		}).on('end', startServer);
-	process.on('message', function (msg) {
-		if (msg.type === 'stats') {
-			process.send({
-				id: msg.id,
-				data: `${stats.clients} clients, ${stats.requests} requests.`
-			});
+let f = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+	props = ['x', 'y', 'x1', 'y1'],
+	stats = {
+		clients: 0,
+		requests: 0
+	},
+	tps = {
+		all: f.length - 1,
+		normal: 35,
+		number: 9
+	},
+	startServer = function (err) {
+		if (err) {
+			console.log('server is stoped by error, restarting...');
 		}
-	}).title = 'fusion captcha server';
-	startServer();
-});
+		net.createConnection(config.server.captcha, function () {
+			this.end();
+			console.log('server is already running');
+		}).on('error', function () {
+			if (typeof config.server.captcha === 'string') {
+				fs.unlink(config.server.captcha, function () {
+					server.listen(config.server.captcha);
+				});
+			} else {
+				server.listen(config.server.captcha);
+			}
+		});
+	},
+	server = net.createServer(function (socket) {
+		let msg = '';
+		socket.on('data', function (data) {
+			data = data.split('\n');
+			for (let i = 0; i < data.length; i++) {
+				msg += data[i];
+				if (i < data.length - 1) {
+					msg = msg.parseJsex();
+					if (msg && dataType(msg.value) === 'object') {
+						let len = msg.value.len || 4,
+							t = tps[msg.value.type] || tps.normal,
+							s = '';
+						for (let i = 0; i < len; i++) {
+							s += f[Math.round(Math.random() * t)];
+						}
+						t = font.getPath(s);
+						t.commands.forEach(function (cmd) {
+							props.forEach(function (v) {
+								if (cmd.hasOwnProperty(v)) {
+									cmd[v] += Math.random() * 4 - 2;
+								}
+							});
+						});
+						msg = {
+							path: t.toPathData(),
+							text: s
+						};
+					} else {
+						msg = Error('bad request');
+					}
+					socket.write(toJsex(msg) + '\n');
+					stats.requests++;
+					msg = '';
+				}
+			}
+		}).on('close', function () {
+			stats.clients--;
+		}).setEncoding('utf8');
+		stats.clients++;
+	}).on('listening', function () {
+		console.log('server started.');
+	}).on('error', function (err) {
+		console.error(err.stack);
+	}).on('end', startServer);
+process.on('message', function (msg) {
+	if (msg.type === 'stats') {
+		process.send({
+			id: msg.id,
+			data: `${stats.clients} clients, ${stats.requests} requests.`
+		});
+	}
+}).title = 'fusion captcha server';
+startServer();
--- config.js
+++ config.js
@@ -1,70 +1,58 @@
-(function (cb) {
-	'use strict';
-	var n, i = 0,
-		prefix = (process.platform === 'win32' ? '\\\\?\\pipe\\' : '/tmp/') + 'fusion.',
-		config = {
-			server: {
-				web: 4443,
-				captcha: prefix + 'server.captcha'
-			},
-			site: {
-				defaultHost: {
-					index: 'index.html',
-					postLen: 64 * 1024,
-					fileLen: 2 * 1024 * 1024,
-					zLen: 4 * 1024,
-					securePath: /^\//,
-					api: {
-						serv: prefix + 'api.defaultHost',
-						deps: ['captcha']
-					}
-				},
-				localhost: {
-					domain: /^localhost$/,
-					api: {
-						serv: prefix + 'api.localhost',
-						deps: ['captcha']
-					}
+'use strict';
+const fs = require('fs'),
+	path = require('path'),
+	prefix = (process.platform === 'win32' ? '\\\\?\\pipe\\' : '/tmp/') + 'fusion.',
+	config = {
+		server: {
+			web: 4443,
+			captcha: prefix + 'server.captcha'
+		},
+		site: {
+			defaultHost: {
+				index: 'index.html',
+				postLen: 64 * 1024,
+				fileLen: 2 * 1024 * 1024,
+				zLen: 4 * 1024,
+				securePath: /^\//,
+				api: {
+					serv: prefix + 'api.defaultHost',
+					deps: ['captcha']
 				}
 			},
-			manager: {
-				port: 4444,
-				password: '123456'
-			}
-		};
-	for (n in config.site) {
-		if (config.site[n].hasOwnProperty('api') && config.site[n].api.hasOwnProperty('deps')) {
-			var j = 0;
-			while (j < config.site[n].api.deps.length) {
-				if (config.server.hasOwnProperty(config.site[n].api.deps[j])) {
-					j++;
-				} else {
-					config.site[n].api.deps.splice(j, 1);
+			localhost: {
+				domain: /^localhost$/,
+				api: {
+					serv: prefix + 'api.localhost',
+					deps: ['captcha']
 				}
 			}
-			if (!config.site[n].api.deps.length) {
-				delete config.site[n].api.deps;
+		},
+		manager: {
+			port: 4444,
+			password: '123456'
+		}
+	};
+for (let n in config.site) {
+	if (config.site[n].hasOwnProperty('api') && config.site[n].api.hasOwnProperty('deps')) {
+		let j = 0;
+		while (j < config.site[n].api.deps.length) {
+			if (config.server.hasOwnProperty(config.site[n].api.deps[j])) {
+				j++;
+			} else {
+				config.site[n].api.deps.splice(j, 1);
 			}
 		}
-		config.site[n].certs = {};
-		getCert(n, 'cert');
-		getCert(n, 'key');
+		if (!config.site[n].api.deps.length) {
+			delete config.site[n].api.deps;
+		}
 	}
-
-	function getCert(site, type) {
-		i++;
-		fs.readFile(path.join(__dirname, 'certs', n, type + '.pem'), {
+	config.site[n].certs = {
+		cert: fs.readFileSync(path.join(__dirname, 'certs', n, 'cert.pem'), {
 			encoding: 'utf8'
-		}, function (err, data) {
-			if (!err) {
-				config.site[site].certs[type] = data;
-			} else {
-				throw err;
-			}
-			i--;
-			if (i === 0) {
-				cb(config);
-			}
-		});
-	}
-})
+		}),
+		key: fs.readFileSync(path.join(__dirname, 'certs', n, 'key.pem'), {
+			encoding: 'utf8'
+		})
+	};
+}
+module.exports = config;
